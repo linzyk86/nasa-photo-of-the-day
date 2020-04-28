c69ad5d91e7b1ff6754dcfc7ea6647f47e2bd13e
Lindsay W <lwhite5314@gmail.com>
2020-04-28 19:31:51 -0400

    photo up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
+
+
+
+
 
 function App() {
+const [pic, setPic] = useState([]);
+
+axios.get("https://api.nasa.gov/planetary/apod?api_key=Hy7B5wdzR0118ZD3T5r5rXNNDkqb0N0zfLuUQKSp")
+  .then(response => {
+    console.log(response);
+    setPic(response.data.url);
+  })
+  .catch(response => {
+    console.log( "error");
+  })
+
   return (
     <div className="App">
       <p>
+        <div>
+          <img src = {pic}></img>
+
+
+        </div>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
